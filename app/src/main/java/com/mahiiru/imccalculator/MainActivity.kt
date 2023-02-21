557f839258df1160e096d228170debf33f94395e
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-21 16:21:53 -0300

    MainActivity functions created.

--- app/src/main/java/com/mahiiru/imccalculator/MainActivity.kt
+++ app/src/main/java/com/mahiiru/imccalculator/MainActivity.kt
@@ -2,10 +2,48 @@ package com.mahiiru.imccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cvMale : CardView
+    private lateinit var cvFemale : CardView
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
+    private lateinit var tvWeight: TextView
+    private lateinit var btnMinusWeight: FloatingActionButton
+    private lateinit var btnAddWeight: FloatingActionButton
+    private lateinit var tvAge: TextView
+    private lateinit var btnMinusAge: FloatingActionButton
+    private lateinit var btnAddAge: FloatingActionButton
+    private lateinit var btnCalculate : Button
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+        initListeners()
+        initUI()
     }
+
+    private fun initComponents() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initListeners() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initUI() {
+        TODO("Not yet implemented")
+    }
+
 }
